14a04fc799b4e4229620e6375c9e3311d9d89808
Olivier Erhard <o.erhard@gmail.com>
2023-01-18 19:04:03 -0300

    Exercicios Feitos: 40,41

---
+++ EX 41.py
@@ -0,0 +1,11 @@
+idade = int(input('IDADE???'))
+if idade <=9:
+    print('Modadalidade MIRIM')
+elif idade > 9 and idade <= 14:
+    print('Modalidade INFANTIL')
+elif idade > 14 and idade <= 19:
+    print('Modalidade JUNIOR')
+elif idade > 19  and idade <= 20:
+    print('Modalidade SENIOR')
+elif idade > 20:
+    print('Modalidade MASTER')
