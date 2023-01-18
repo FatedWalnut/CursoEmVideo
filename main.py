71c891169dd58142eae985c23f5aa9b59da36e1a
Olivier Erhard <o.erhard@gmail.com>
2023-01-18 18:52:15 -0300

    Ex 40

---
+++ main.py
@@ -0,0 +1,9 @@
+'''nota1 = int(input('primeira nota? '))
+nota2 = int(input('segunda nota ?'))
+media = (nota1 + nota2)/2
+if media >= 7:
+    print('Aluno aprovado')
+elif media < 5:
+    print('Aluno reprovado')
+elif media >= 5 and media < 7:
+    print('Aluno em recuperação')'''
