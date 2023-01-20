06c1618eca57e826b8972f026f6fbe0195b5ae4e
Olivier Erhard <o.erhard@gmail.com>
2023-01-20 18:45:38 -0300

    Exercicio Feito: 63, 64

--- EX64.py
+++ EX64.py
@@ -2,7 +2,7 @@ num = 0
 cont = 0
 soma = 0
 while num != 999:
-    num = int(input('ESCOLHA UM NUMERO:'))
+    num = int(input('ESCOLHA UM NUMERO [999 PARA]:'))
     if num == 999:
         break
     soma += num
